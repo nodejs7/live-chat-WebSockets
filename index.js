246276b944a1d2e343ab39a2d347938ef4ad25a6
Americanman <snowboardracer7@gmail.com>
2021-02-26 15:55:32 -0500

    Implement socket connetion

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var socket = require('socket.io')
 
 // initialize(set up) App
 var app = express();
@@ -7,3 +8,8 @@ var server = app.listen(4000, () => console.log('Listening to requests on port 4
 
 // Static files
 app.use(express.static('public'))
+
+// Socket setup
+var io = socket(server);
+
+io.on('connection', socket => console.log('made socket connection'))
